9d100f8107cd530465a8c4bdc7f2103c110258e9
Bill Roy <bill@bitlash.net>
2010-12-27 11:36:10 -0700

    stack bug fix

--- src/bitlash-parser.c
+++ src/bitlash-parser.c
@@ -56,7 +56,7 @@ prog_char strings[] PROGMEM = {
 #ifdef TINY85
 	"exp \0unexp \0mssng \0str\0 uflow \0oflow \0\0\0\0exp\0op\0\0eof\0var\0num\0)\0\0eep\0:=\"\0> \0line\0char\0stack\0startup\0id\0prompt\0\r\n\0\0\0"
 #else
-	"expected \0unexpected \0missing \0string\0 underflow\0 overflow\0^C\0^B\0^U\0exp\0op\0:xby+-*/\0eof\0var\0number\0)\0saved\0eeprom\0 = \"\0> \0line\0char\0stack\0startup\0id\0prompt\0\r\nFunctions:\0oops\0arg\0"
+	"expected \0unexpected \0missing \0string\0 underflow\0 overflow\0^C\0^B\0^U\0exp\0op\0:xby+-*/\0eof\0var\0number\0)\0saved\0eeprom\0:=\"\0> \0line\0char\0stack\0startup\0id\0prompt\0\r\nFunctions:\0oops\0arg\0"
 #endif
 };
 
@@ -631,16 +631,20 @@ byte thesym = sym;
 
 		// Macro-returning-value used as a factor
 		case s_macro:				// macro returning value
-			if (sym == s_define) defineMacro();
+			if (sym == s_define) {
+				defineMacro();
+				vpush(0);
+			}
 			else domacrocall(thesymval);	// call the macro; its value is on the stack
 			break;
 
+		// undefined symbol: define macro
 		case s_undef:
 			if (sym != s_define) unexpected(M_id);
 			defineMacro();
+			vpush(0);
 			break;
 
-#ifdef ARDUINO_BUILD
 		case s_apin:					// analog pin reference like a0
 			if (sym == s_equals) { 		// digitalWrite or analogWrite
 				getsym();
@@ -649,7 +653,7 @@ byte thesym = sym;
 			}
 			else vpush(analogRead(thesymval));
 			break;
-#endif
+
 		case s_dpin:					// digital pin reference like d1
 			if (sym == s_equals) { 		// digitalWrite or analogWrite
 				getsym();
