8271ffc921ac73a255d01912cccd1bd8224726e5
murix <murilo.pontes@gmail.com>
2014-02-05 16:21:24 -0300

    Extend Stellaris and Tiva C launchpad * Implement software in bitbash boot command * Implement EEPROM support eeinit(), eewrite(), eeread()

--- src/bitlash-interpreter.c
+++ src/bitlash-interpreter.c
@@ -82,7 +82,9 @@ void cmd_boot(void) {
 }
 #elif ARM_BUILD==4
 void cmd_boot(void){
-  oops('boot');
+  //oops('boot');
+  HWREG(NVIC_APINT) = NVIC_APINT_VECTKEY | NVIC_APINT_SYSRESETREQ;
+  while(1);
 }
 #else
 void cmd_boot(void) {
